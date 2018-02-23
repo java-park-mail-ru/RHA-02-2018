a4745f38ca63892b38afac373af845a9d9ab620e
HimmelSpark <adam404pet@gmail.com>
2018-02-23 17:42:18 +0300

    start

---
+++ src/main/java/com/gameapi/rha/RhaApplication.java
@@ -0,0 +1,12 @@
+package com.gameapi.rha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhaApplication.class, args);
+	}
+}
